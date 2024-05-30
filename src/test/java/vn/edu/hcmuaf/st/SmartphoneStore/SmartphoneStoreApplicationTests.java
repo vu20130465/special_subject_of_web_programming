9db1f67325aab29f41a9d1143a66d320a5749446
vu20130465 <20130465@st.hcmuaf.edu.vn>
2024-05-30 21:26:34 +0700

    Create project

---
+++ src/test/java/vn/edu/hcmuaf/st/SmartphoneStore/SmartphoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.st.SmartphoneStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartphoneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
